aac539ed1bf696ab58e85fe3e836223df2edde8b
sunzy <1003796668@qq.com>
2016-08-19 16:26:33 +0800

    2016-08-19-16:26

---
+++ push.sh
@@ -0,0 +1,27 @@
+#！usr/bin/bash
+#push branch to github branch
+#branchName=`git rev-parse --abbrev-ref HEAD`
+#echo "git branch:$branchName"
+git add --all .
+message=`date +%Y-%m-%d-%H:%M`
+while getopts m: opt
+do
+    case $opt in
+        m)
+            message=$OPTARG
+            ;;
+        ?)
+            echo "Usage:args [-m]"
+            echo "-m means message"
+            echo "exit"
+            exit
+            ;;
+    esac
+done
+git commit -m "$message"
+#git fetch github
+#git merge --no-edit github/master
+git commit -m "merge $message"
+remote=`git remote`
+echo "push to:$remote"
+git push lab $branchName
